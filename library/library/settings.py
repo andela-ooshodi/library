fd01e9ff9b2f486097b51ab3bb1fab8ff38b9d28
Olufunmilade Oshodi <olufunmilade.oshodi@andela.com>
2017-04-09 13:23:37 +0100

    remove django nose

--- library/library/settings.py
+++ library/library/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf.apps.BookshelfConfig',
-    'django_nose'
 ]
 
 MIDDLEWARE = [
@@ -124,11 +123,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# django nose
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-NOSE_ARGS = [
-    '--with-coverage',
-    '--cover-html',
-    '--cover-package=bookshelf',
-]
