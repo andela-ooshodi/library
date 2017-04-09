45fcd6acde8503e9df44e042135cd15b183af1a3
Olufunmilade Oshodi <olufunmilade.oshodi@andela.com>
2017-04-09 14:29:41 +0100

    library view and admin

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView, View
+from .models import Category, Book
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    # Base view
+    template_name = 'bookshelf/base.html'
+
+
+class ResultView(View):
+    # Result view
+    # Function that handles the get request received in this view
+    def get(self, request):
+        name = request.GET.get('name')
+
+        # query both tables for similar names
+        category = Category.objects.filter(name__icontains=name)
+        book = Book.objects.filter(name__icontains=name)
+
+        if any([category, book]):
+            # payload
+            payload = {'categories': category,
+                       'books': book}
+            return render(request, 'bookshelf/result.html', payload)
+        else:
+            return redirect(reverse('404'))
+
+
+class NotFoundView(TemplateView):
+    # 404 view
+    template_name = 'bookshelf/404.html'
