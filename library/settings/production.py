c8bf5b1d515f8ef33e3cfab09816ef249ff4e749
Olufunmilade Oshodi <olufunmilade.oshodi@andela.com>
2017-04-15 13:45:29 +0100

    Fixing deploy

--- library/settings/production.py
+++ library/settings/production.py
@@ -11,7 +11,4 @@ DATABASES = {
 # Enable Persistent Connections
 DATABASES['default']['CONN_MAX_AGE'] = 500
 
-# Enable Connection Pooling
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
-
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
