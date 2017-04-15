163386435b552db604bf842086df891bb368f011
Olufunmilade Oshodi <olufunmilade.oshodi@andela.com>
2017-04-15 14:12:59 +0100

    Update circle.yml to fix deploy again

--- library/settings/__init__.py
+++ library/settings/__init__.py
@@ -7,8 +7,10 @@ dotenv_path = join(dirname(__file__), '../..', '.env')
 load_dotenv(dotenv_path)
 
 if os.environ.get("LOCAL_DEV") or os.environ.get("CIRCLE_CI"):
+    print "loading development settings"
     from .development import *
 elif os.environ.get("HEROKU"):
+    print "loading production settings"
     from .production import *
 else:
     print "Error determining environment"
