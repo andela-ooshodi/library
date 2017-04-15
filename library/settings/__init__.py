cb090e98ac6e69cccbc66905b8b856c7dbc66f7e
Olufunmilade Oshodi <olufunmilade.oshodi@andela.com>
2017-04-15 12:37:14 +0100

    [PLAT-262] setup app for production

---
+++ library/settings/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), './../../.env')
+load_dotenv(dotenv_path)
+
+if os.environ.get("LOCAL_DEV") or os.environ.get("CIRCLE_CI"):
+    from .development import *
+elif os.environ.get("HEROKU"):
+    from .production import *
+else:
+    print "Error determining environment"
