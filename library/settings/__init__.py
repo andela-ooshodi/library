24500e75cb685ec49dc8c4abd6aaca92180258ad
Olufunmilade Oshodi <olufunmilade.oshodi@andela.com>
2017-04-15 13:28:23 +0100

    Fix deploy

--- library/settings/__init__.py
+++ library/settings/__init__.py
@@ -3,7 +3,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), './../../.env')
+dotenv_path = join(dirname(__file__), '../..', '.env')
 load_dotenv(dotenv_path)
 
 if os.environ.get("LOCAL_DEV") or os.environ.get("CIRCLE_CI"):
