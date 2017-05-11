037263916bc470e663210579d7927f7b9271f8f3
Olufunmilade Oshodi <olufunmilade.oshodi@andela.com>
2017-05-11 01:25:25 +0100

    Filter by query param

--- libraryapi/urls.py
+++ libraryapi/urls.py
@@ -5,8 +5,8 @@ from libraryapi.views import CategoryViewSet, BookViewSet
 
 # register our v1 api URL pattern
 v1_router = routers.DefaultRouter()
-v1_router.register(r'categories', CategoryViewSet)
-v1_router.register(r'books', BookViewSet)
+v1_router.register(r'categories', CategoryViewSet, base_name='category')
+v1_router.register(r'books', BookViewSet, base_name='book')
 
 urlpatterns = [
     url(r'^v1/', include(v1_router.urls)),
--- libraryapi/views.py
+++ libraryapi/views.py
@@ -6,11 +6,29 @@ from .serializers import CategorySerializer, BookSerializer
 
 
 class CategoryViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
-    queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        """
+        Optionally retrieve/list categories by its name
+        """
+        queryset = Category.objects.all()
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
 
 
 class BookViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_queryset(self):
+        """
+        Optionally retrieve/list books by its name
+        """
+        queryset = Book.objects.all()
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
 
